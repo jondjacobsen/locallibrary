5a52a54be2a88354f500e83d68c339630fdb60fc
Jonathan Jacobsen <jonathan.jacobsen@gmail.com>
2019-02-07 06:06:57 -0500

    Updated Admin classes

--- catalog/admin.py
+++ catalog/admin.py
@@ -4,8 +4,23 @@ from django.contrib import admin
 
 from catalog.models import Author, Genre, Book, BookInstance, Language
 
+#Define the admin class
+class AuthorAdmin(admin.ModelAdmin):
+    pass
+
+#Register the admin class with the associated model
+admin.site.register(Author, AuthorAdmin)
+
+# Register the Admin classes for Book using the decorator
 admin.site.register(Book)
-admin.site.register(Author)
-admin.site.register(Genre)
+class BookAdmin(admin.ModelAdmin):
+    pass
+
+# Register the Admin classes for BookInstance using the decorator
 admin.site.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin)
+    pass
+
+
 admin.site.register(Language)
+admin.site.register(Genre)
