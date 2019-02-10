922cd5b68a8c0da69f3e1732ecf527e88df49f90
Jonathan Jacobsen <jonathan.jacobsen@gmail.com>
2019-02-09 22:24:25 -0500

    Admin - Book Instance Update

--- catalog/admin.py
+++ catalog/admin.py
@@ -7,6 +7,7 @@ from catalog.models import Author, Genre, Book, BookInstance, Language
 #Define the admin class
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
 
 #Register the admin class with the associated model
 admin.site.register(Author, AuthorAdmin)
@@ -21,8 +22,18 @@ admin.site.register(Book, BookAdmin)
 # Register the Admin classes for BookInstance using the decorator
 
 class BookInstanceAdmin(admin.ModelAdmin):
-    list_display = ('book', 'status', 'due_back')
+    list_display = ('book', 'imprint', 'status')
     list_filter = ('status', 'due_back')
+
+    fieldsets = (
+        (None, {
+            'fields': ('book', 'imprint', 'id')
+        }),
+        ('Availability', {
+            'fields': ('status', 'due_back')
+        }),
+    )
+
 admin.site.register(BookInstance, BookInstanceAdmin)
 
 admin.site.register(Language)
