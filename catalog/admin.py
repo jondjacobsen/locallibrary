2d94fedc2b6c531f918019e07e850293853fa33c
Jonathan Jacobsen <jonathan.jacobsen@gmail.com>
2019-02-10 08:18:11 -0500

    Admin Site Improvements

--- catalog/admin.py
+++ catalog/admin.py
@@ -5,19 +5,29 @@ from django.contrib import admin
 from catalog.models import Author, Genre, Book, BookInstance, Language
 
 #Define the admin class
+class BookAdminInline(admin.TabularInline):
+    model = Book
+    extra = 0
+
+@admin.register(Author)
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
     fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+    inlines = [BookAdminInline]
 
 #Register the admin class with the associated model
-admin.site.register(Author, AuthorAdmin)
 
 # Register the Admin classes for Book using the decorator
 
+class BookInstanceInline(admin.TabularInline):
+    model = BookInstance
+    extra = 0
+
+@admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'display_genre')
+    inlines = [BookInstanceInline]
 
-admin.site.register(Book, BookAdmin)
 
 # Register the Admin classes for BookInstance using the decorator
 
