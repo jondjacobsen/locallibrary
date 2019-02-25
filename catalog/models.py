b955b50538c57a8bda639747f4c57fd4b1f4ce8a
Jonathan Jacobsen <jonathan.jacobsen@gmail.com>
2019-02-24 21:34:09 -0500

    Add - Book Detail View

--- catalog/models.py
+++ catalog/models.py
@@ -52,6 +52,10 @@ class Book(models.Model):
 
     display_genre.short_description = 'Genre'
 
+    def get_absolute_url(self):
+        """Returns the url to access a particular book instance."""
+        return reverse('book-detail', args=[str(self.id)])
+
     def __str__(self):
         """String for representing the Model object."""
         return self.title
